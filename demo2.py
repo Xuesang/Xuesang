da702f24025acd64fdab57c0a7a975948e58f194
xuesang <815115643@qq.com>
2023-02-20 16:51:51 +0800

    add .

--- demo2.py
+++ demo2.py
@@ -1,8 +1,8 @@
 a = [12,454,67,3]
 for i in range(len(a)-1):
-	for j i range(len(a)-1-i):
-		if a[j]>a[j+1+i]
-		b = a[j]
-		a[j] = a[j+1+i]
+	for i in range(len(a)-1-i):
+		if a[i]>a[j+1+i]
+		b = a[i]
+		a[i] = a[j+1+i]
 		a[j+1+i] = b
 print(a)
